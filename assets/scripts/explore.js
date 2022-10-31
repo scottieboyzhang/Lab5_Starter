2d588e07adfbfc2c66b808d228c85f7e820c9e12
scottieboyzhang <97482103+scottieboyzhang@users.noreply.github.com>
2022-10-30 22:12:45 -0700

    change smile face

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,36 +3,37 @@
 window.addEventListener('DOMContentLoaded', init);
 const synth = window.speechSynthesis;
 var voices=[];
+
+
 function init() {
   const butt = document.querySelector('button');
   const inputTxt = document.getElementById("text-to-speak");
   const voiceSelect = document.querySelector('select');
-  
+  var result = document.querySelectorAll("img")[0];
   populateVoiceList();
   if (typeof speechSynthesis !== 'undefined' && speechSynthesis.onvoiceschanged !== undefined) {
     speechSynthesis.onvoiceschanged = populateVoiceList;
   }
-  
-  butt.onclick = (event) =>{
-    const utterThis = new SpeechSynthesisUtterance(inputTxt.value);
-    const utterNull = new SpeechSynthesisUtterance("a");
-    const selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
-    
+
+  butt.addEventListener('click', () =>{
+    result.setAttribute('src', 'assets/images/smiling-open.png');
+    let utterThis = new SpeechSynthesisUtterance(inputTxt.value);
+    let selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
     for (let i = 0; i < voices.length; i++) {
       if (voices[i].name === selectedOption) {
         utterThis.voice = voices[i];
-        synth.speak(utterThis);
-        var result = document.querySelector("img");
-        result.src = "assets/images/smiling-open.png"
       }
     }
-    inputTxt.blur(); 
-  }
+    synth.speak(utterThis);
+    utterThis.addEventListener('end', () => {
+      result.setAttribute('src', 'assets/images/smiling.png');
+    });
+   inputTxt.blur(); 
+  })
 }  
   
 function populateVoiceList() {
   voices = synth.getVoices();
-  
   if (typeof speechSynthesis === 'undefined') {
     return;
   }
@@ -47,7 +48,6 @@ function populateVoiceList() {
     option.setAttribute('data-name', voices[i].name);
     document.querySelector("select").appendChild(option);
   }
-  
 }
 
 
